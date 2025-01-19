d758daea1f9c882789e95c8c15e81c107e42b68c
mhmdnurfauzan <154282921+mhmdnurfauzan@users.noreply.github.com>
2025-01-19 23:38:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        name: 'Fauzan',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        score: {
+            HTML: 90,
+            CSS: 80,
+            JS: 85
+        }
+    }
+}
